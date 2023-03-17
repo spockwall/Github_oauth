891d87f0dc772ee43c096de4bffda7671c5b9d9d
spockwall <abcdefg35216874@gmail.com>
2023-03-17 22:10:49 +0800

    basic environment setting

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return <div>
+    hello
+  </div>
+
+}
+
+export default App
